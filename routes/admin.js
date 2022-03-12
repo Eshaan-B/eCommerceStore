22d3460add26a1f2fafef7bbd0b4135d2740ffbd
Eshaan-B <eshaan.bhardy@gmail.com>
2022-03-12 11:38:07 +0530

    styled pages

--- routes/admin.js
+++ routes/admin.js
@@ -5,16 +5,23 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const products=[];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
+  console.log(products);
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+ 
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({title:req.body.title});
+  console.log(products);
   res.redirect('/');
 });
 
-module.exports = router;
+module.exports={
+    router:router,
+    products:products
+}
--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,13 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData=require('./admin');
+const admin = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  console.log(admin.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
